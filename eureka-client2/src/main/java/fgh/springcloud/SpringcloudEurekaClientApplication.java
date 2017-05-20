bfa1286d28a160645ce664b0aef0eae8944e8dd1
ghsy158 <fgh326@126.com>
2017-05-20 19:59:02 +0800

    eureka client

---
+++ eureka-client2/src/main/java/fgh/springcloud/SpringcloudEurekaClientApplication.java
@@ -0,0 +1,30 @@
+package fgh.springcloud;
+
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringcloudEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaClientApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@Hystrix
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name){
+		return "hi "+name+",i am from port:" +port;
+	}
+}
---
+++ service-ribbon/src/main/java/fgh/springcloud/HelloService.java
@@ -0,0 +1,18 @@
+package fgh.springcloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by huxia on 2017/5/20.
+ */
+@Service
+public class HelloService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name){
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+}
