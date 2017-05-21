9cb24528c6e0cfc1dee38b8de63b49961ff6a35e
ghsy158 <fgh326@126.com>
2017-05-21 23:47:24 +0800

    zuul 路由配置

---
+++ service-zuul/src/main/java/fgh/springcloud/ServiceZuulApplication.java
@@ -0,0 +1,18 @@
+package fgh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+//在其入口applicaton类加上注解@EnableZuulProxy，开启zuul：
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZuulApplication.class, args);
+	}
+}
