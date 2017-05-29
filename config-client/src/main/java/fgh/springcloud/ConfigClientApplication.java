90053c866a3ce2f16608d1147101e76e68f1ffb2
fengguanghui <fengguanghui@huaxiapawn.com>
2017-05-29 14:52:23 +0800

    使用mq动态刷新配置文件的修改，如果配置文件修改了，使用post方式调用以下接口获取最新的内容:http://localhost:8881/bus/refresh

--- config-client/src/main/java/fgh/springcloud/ConfigClientApplication.java
+++ config-client/src/main/java/fgh/springcloud/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package fgh.springcloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+//RefreshScope:当配置文件发生变化时，不用重启应用会自动更新
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
