116ef28d8e9c17e07d186ba3769999f05f2752df
fengguanghui <fengguanghui@huaxiapawn.com>
2017-05-29 14:48:14 +0800

    用AMQP实现通知微服务架构的配置文件的更改

---
+++ config-client2/src/main/java/fgh/springcloud/ConfigClientApplication.java
@@ -0,0 +1,30 @@
+package fgh.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+//RefreshScope:当配置文件发生变化时，不用重启应用会自动更新
+@RefreshScope
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public  String hi(){
+		System.out.println("foo==="+foo);
+		return foo;
+	}
+}
